368d87a2d9d369d25027534511b8219a5a1f9457
STD\s20225010 <wata.kuma@hotmail.co.jp>
2023-12-01 11:11:53 +0900

    no message

--- Assets/Scripts/GameStateMachineScript.cs
+++ Assets/Scripts/GameStateMachineScript.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class GameStateMachineScript
 {
     private BaseGameStateScript _nowState;
-    public GameStateMachineScript()
+    public GameStateMachineScript(BaseGameStateScript initState)
     {
-        
+        _nowState = initState;
     }
     /// <summary>
     /// ó‘Ô‚ğ•Ï‚¦‚ê‚éê‡‚Í•Ï‚¦‚é
     /// </summary>
     public void UpdateState()
     {
-
+        
     }
     /// <summary>
     /// Œ»İ‚Ìó‘Ô‚Å‚Ì“®ì‚ğÀs‚·‚é
     /// </summary>
     public void Execute()
     {
-
+        _nowState.Execute();
     }
 }
