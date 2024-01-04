506d4f18857d5e6e7e9329905f47cdfc26100537
DESKTOP-9OKINNC\kuma <wata.kuma@hotmail.co.jp>
2024-01-04 21:43:34 +0900

    no message

--- Assets/Scripts/InGameState/InGameStateMachineScript.cs
+++ Assets/Scripts/InGameState/InGameStateMachineScript.cs
@@ -3,7 +3,7 @@
 /// </summary>
 public class InGameStateMachineScript
 {
-    private BaseInGameStateScript _beforeState = default;
+    private BaseInGameStateScript _nowState = default;
     private IInputPlayerAction _inputPlayerAction = default;
     /// <summary>
     /// ステートを更新する
@@ -13,13 +13,25 @@ public class InGameStateMachineScript
     {
         if (_inputPlayerAction.IsOpenInventory())
         {
-
+            //プレイ中だったら
+            if(_nowState.GetType() == typeof(PlayStateScript))
+            {
+                _nowState = new InventoryStateScript();
+			}
+			else if(_nowState.GetType() == typeof(InventoryStateScript))
+			{
+                _nowState = new PlayStateScript();
+			}
         }else if (_inputPlayerAction.IsOpenPose())
         {
-
+            if(_nowState.GetType() == typeof(PlayStateScript))
+			{
+                _nowState = new PoseStateScript();
+			}else if (_nowState.GetType() == typeof(PoseStateScript))
+			{
+                _nowState = new PlayStateScript();
+			}
         }
-        
-
-        return null;
+        return _nowState;
     }
 }
